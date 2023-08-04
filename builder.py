34cccd76c9bd5dbac780189460d4e23c09353297
ToSauced <69777921+ToSauced@users.noreply.github.com>
2023-08-04 15:41:31 -0400

    Update builder.py

--- builder.py
+++ builder.py
@@ -6,6 +6,25 @@ import tutilities.basic as basic
 current_dir = basic.get_directory(__file__)
 dir_toBuild = f'{current_dir}toBuild{os.sep}'
 
+# Update client.py (direct file edits) with updated connection values 
+try:
+        with open (f'') as f:
+                for line in f:
+                        if 'host = ' in str(line):
+                                data = basic.fileRead(f'{dir_toBuild}client.py').replace(line, f"host = {build.get('host')}\n")
+                                basic.fileWrite(f'{dir_toBuild}client.py',data)
+                        if 'port = ' in str(line):
+                                data = basic.fileRead(f'{dir_toBuild}client.py').replace(line, f"port = {build.get('port')}\n")
+                                basic.fileWrite(f'{dir_toBuild}client.py',data)
+                                
+except Exception as e: print(e)
+
+proj_name = "grayscale"
+exe_name = "grayscale"
+description = "Gangsters Real Edition"
+                                
+        
+
 include_files = [f'{dir_toBuild}favicon.ico',]
 base = None
 
@@ -13,15 +32,15 @@ base = None
 #    base = "Win32GUI"
 
 def main():
-        setup(name="grayscaleworkshop",
+        setup(name=proj_name,
         version="1.0",
-        description="grayscaleworkshop-client.exe",
+        description=f"{description}",
         options={'build_exe': {'include_files': include_files}},
         executables=[Executable(
             f'{dir_toBuild}client.py',
             base=base,
             icon=f'{dir_toBuild}favicon.ico',
-            target_name='grayscale-client.exe')])
+            target_name=f'{exe_name}.exe')])
 
 if __name__ == '__main__':
     main()
